a85d670fee2981b0ccff7d11e8c4cc44e34cd761
karthik rm <rmkarthik@hotmail.com>
2019-07-25 18:07:34 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle (todo){
+    todo.completed = !todo.completed;
+  }
+
+  onDelete (todo){
+    this.deleteTodo.emit(todo);
+  }
+}
